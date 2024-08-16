e4ce7ea0bf79dde34ec70238bc871cd0a7ec76d2
Otaciliojunior86 <otajunior1986@gmail.com>
2024-08-16 08:29:19 -0300

    Atualize

---
+++ meu/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Loja 7</h1>
+      <h2>O seu estilo comeca aqui</h2>
+
+    </div>
+  );
+}
+
+export default App;
